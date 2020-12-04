f7791fc81a43699e226f534d93b0568443fec6aa
Mohamed EL HABNOUNI <mohamed.elhabnouni@viacesi.fr>
2020-12-04 11:51:11 +0100

    facture

---
+++ projet_cbdd/Facture1.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace projetcbdd {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Description résumée de Facture
+	/// </summary>
+	public ref class Facture : public System::Windows::Forms::Form
+	{
+	public:
+		Facture(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~Facture()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Facture
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(877, 704);
+			this->Name = L"Facture";
+			this->Text = L"Facture";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
